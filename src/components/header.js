03a1b5695ce2f723c5f1c4c83c0caabbc1020e3a
Hamood <mumualafifi@gmail.com>
2022-09-27 17:16:22 -0400

    Header and home began
    Added beginner basic header component and home page JS and css to build upon.
    Error (probably with importing theme.css).

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import { Box } from '@mui/system';
+import "./header.css";
 
-export const header = () => {
+export const Header = () => {
     return(
 
-        <Box>
-            hi
+        <Box className="bar">
+            <div>
+                ----------------------------hi----------------------------
+            </div>
         </Box>
         
     )
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,17 +1,13 @@
 import { Sidebar } from "../../components/sidebar";
 import { Header } from "../../components/header";
 import { Box, Button } from "@mui/material";
+import "./home.css";
 
 export const HomePage = () => {
     return (
-        
-        <Box sx = {{ display: "flex", backgroundColor: "red"}}>
-            {/* sidebar */}
-            <Box sx = {{ display: "inline-block", position: "fixed"}}>
-                {/* <Header/> */}
-                <Sidebar/>
-            </Box>
-            
+        <Box sx = {{ display: "inline-block", position: "fixed"}}>
+            <Header/>
+            <Sidebar/>
         </Box>
     )
 };
