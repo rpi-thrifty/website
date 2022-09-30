e06e31f68ae9fd4a362fcb39ff06f158d524d7a1
Ardennt <jiaweiw02@gmail.com>
2022-09-30 17:25:26 -0400

    added some stock images, sidebar and top bar complete

---
+++ src/components/sidebar.js
@@ -0,0 +1,66 @@
+import React, {useState} from 'react';
+import { Box, Button, IconButton, Typography } from '@mui/material';
+import ListIcon from '@mui/icons-material/List';
+import { Link } from 'react-router-dom';
+import { color } from '@mui/system';
+
+export const Sidebar = () => {
+    // hide or show the sidebar
+    const [showing, setShowing] = useState(false);
+    const showSideBar = () => setShowing(!showing);
+
+    const button_sidebar = (name) => {
+        return <Button sx = {{ display: "block", margin: "2vh 0vw 2vh 2vw", color: "#57201B", fontSize: "1.5vh" }}>
+            {name}
+        </Button>
+    }
+
+    const showing_sidebar = () => {
+        return <Box sx = {{
+            height: "91vh", 
+            component: "span", 
+            width: "18vw", 
+            display: "block", 
+            backgroundColor: "#C8AFAB",
+            borderRadius: "15px"}}>
+
+            <IconButton onClick = {showSideBar}>
+                <ListIcon sx = {{fontSize: "4vh", color: "#57201B"}}/>
+            </IconButton>
+            <>{button_sidebar("Home")}</>
+            <>{button_sidebar("Categories")}</>
+        </Box>
+    }
+
+    const hidden_sidebar = () => {
+        return <Box sx = {{height: "100%", 
+            width: "18vw", 
+            backgroundColor: "#C8AFAB", 
+            borderRadius: "15px"}}>
+
+            <IconButton onClick = {showSideBar}>
+                <ListIcon sx = {{fontSize: "4vh"}}/>
+            </IconButton>
+        </Box>
+    }
+
+    return (
+        <>
+            <Box>
+                {
+                    // if the sidebar is being shown
+                    showing ? 
+                    <>
+                    {showing_sidebar()}
+                    </>
+                    :
+                    // sidebar is not being shown
+                    <>
+                    {hidden_sidebar()}
+                    </>
+
+                }
+            </Box>
+        </>
+    )
+}
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,15 +1,49 @@
-import { Sidebar } from "../../components/sidebar/sidebar";
+import { Sidebar } from "../../components/sidebar";
 import { Header } from "../../components/header";
-import { Box, Button } from "@mui/material";
+import { Box, Button, Typography, borderRadius } from "@mui/material";
+import ITEM_ENTRY from "../../components/entry";
+
 
 export const HomePage = () => {
+
+    const entryList = (entryL) => {
+        for (let i = 0; i < entryL.length; i++){ 
+            
+        }
+    }
+
     return (
-        
-        <Box sx = {{ display: "flex", backgroundColor: "red"}}>
-            {/* sidebar */}
-            <Box sx = {{ display: "inline-block", position: "fixed"}}>
-                {/* <Header/> */}
-                <Sidebar/>
+        // outside box
+        <Box sx = {{
+            display: "flex",
+            justifyContent: "space-between",
+            margin: "9vh 0 0 20vw"
+        }}>
+            <Box sx = {{
+                width: "100vw", 
+                left: "0",
+                top: "0",
+                height: "7vh", 
+                backgroundColor: "#E4D6D4", 
+                position: "fixed"
+            }}>
+                {/* sidebar */}
+                <Box sx = {{ display: "inline-block", 
+                    position: "fixed",
+                    left: "1vw",
+                    top: "8vh",
+                    width: "18vw",}}>
+                    <Sidebar/>
+                </Box>
+
+                {/* insert header behind sidebar */}
+                <Box>
+                    <Typography> text for sidebar goes here </Typography>
+                </Box>
+            </Box>
+
+            <Box>
+                <img src="../../../images/1.jpg"></img>
             </Box>
             
         </Box>
