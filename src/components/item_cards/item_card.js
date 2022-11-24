fc33ecd4a59640508b8920cd2899101ab62f4569
Hamood <mumualafifi@gmail.com>
2022-11-23 19:49:02 -0500

    component for items

--- src/components/item_cards/item_card.js
+++ src/components/item_cards/item_card.js
@@ -1,11 +1,30 @@
-import * as React from 'react';
-import { Box } from '@mui/system';
-// import {logoLong} from "/images/logoLong.jpg";
+import React from 'react'
 
-export const Card = () => {
-    return(
-        <div>
-            
-        </div>
+function Card(props) {
+    return (
+        <Box className="img-div">
+            <a href={props.url} className="link">
+                {/* image */}
+                <Box sx={{display: "flex", justifyContent: "center", marginTop: "1vh"}}>
+                    <img className="item" src={props.img_path}/>
+                </Box>
+                {/* text */}
+                <Box sx={{display: "block", width: "200px", height: "70px", margin: "10px 0 0 12.5px", overflow: "hidden"}}>
+                    {
+                        <Typography sx={{
+                            fontWeight: "bold", 
+                            fontSize:"h6.fontSize", 
+                            fontFamily: "Helvetica Neue", 
+                            textTransform:"uppercase"
+                        }}>
+                            {typeof props.price == 0 ? <>${props.price}</> : <>FREE</>}
+                        </Typography>
+                    }
+                    <Typography sx={{fontSize: "11px", fontFamily: "Helvetica Neue", lineHeight: "1"}}>{props.desc}</Typography>
+                </Box>
+            </a>
+        </Box>
     )
 }
+
+export default Card
