6a5e05cc83ac0ced5dd68fb35a44b255a82b5df0
Alex Rougebec <arougebec22@gmail.com>
2023-02-28 17:40:56 -0500

    about page
    completed about page with Raleway font

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,67 @@
 import { Header } from "../components/header/header";
-import { Box } from "@mui/material"
+import { Box, Button } from "@mui/material"
+import "./about.css"
 
 export const AboutPage = () => {
     return(
-        <Box sx={{marginLeft:"2.5%"}}>
+        <Box sx={{marginLeft:"2.55%"}}> 
             <Header/>
-            <div>
-                Us.
+            <div class = "row " id = "sell-section">
+                <div class = "sell-section-left">
+                    <img class="sell-image-left" src="/images/sell.jpg" alt="Sell"/>
+                </div>
+                <div class = "sell-section-right">
+                    <p id="sell text"> Sell an Item! </p>
+                    <Button class="sell-button">Sell</Button>
+                </div>
+            </div>
+            <div class = "row" id = "buy-section">
+                <div class = "buy-section-left">
+                    <p id="buy text"> Buy an Item! </p>
+                    <Button class="buy-button">Buy</Button>
+                </div>
+                <div class ="buy-section-right">
+                    <img class="buy-image-right" src="/images/buy.jpg" alt="Buy"/>
+                </div>
+            </div>
+            <div class = "row" id = "project-description">
+                <div class = "project-wrapper">
+                <p><b>  Welcome to Thrifty! We are an ecommerce website created 
+                    to facilitate the many transactions made between RPI students 
+                    annually. Our vision this year is to create a 
+                    platform where students can post miscellaneous 
+                    items and find interested buyers. This project 
+                    creates a solution for graduating 
+                    students who have extra school supplies and little storage space
+                    as well as freshmen with a tight budget. This allows freshmen to 
+                    save money by buying second hand products at a discounted price, 
+                    sort of like a permanent online garage sale.
+                </b></p>
+                </div>
+            </div>
+            <div class = "row" id = "about-section">
+                <div class = "members-box">
+                    <h3 id="team-members-title">Team Members: </h3>
+                    <ul>
+                        <li>Alex Rougebec</li>
+                        <li>Jiawei Wu</li>
+                        <li>Nikhil Belgaonkar</li>
+                        <li>Jericho Dizon</li>
+                        <li>Andrew Hua</li>
+                    </ul>
+                </div>
+                <div class="contact-wrapper">
+                    <div class = "contact-box">
+                        <a href = "https://github.com/rpi-thrifty/website">
+                            <img class="aboutimg" src="/images/GitHub-Mark.png" alt="github"/>
+                        </a>
+                        <a href = "https://google.com">
+                            <img class="aboutimg" src="/images/discord.png" alt="discord"/>
+                        </a>
+                    </div>
+                </div>
             </div>
         </Box>
+            
     )
 }
