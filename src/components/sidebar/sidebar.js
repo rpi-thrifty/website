d8831147f355dec43ffff43c5e28e36419d7e84a
JerichoDizon <dizonj@rpi.edu>
2023-03-17 18:03:55 -0400

    Image Slider

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,63 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import { Box, Button, Typography } from '@mui/material';
-import ListIcon from '@mui/icons-material/List'
-import HomeIcon from '@mui/icons-material/Home';
-import CategoryIcon from '@mui/icons-material/Category';
-
-
-export const SIDEBAR = ({handleShift}) => {
-
-    // make the categories slide down
-    // allow for selections
-
-    const button_sidebar = (name) => {
-        return <Button sx = {{ display: "block", margin: "2vh 0vw 2vh 1vw", color: "#57201B", fontSize: "1.5vh" }}>
-            <Box sx={{display:"flex", justifyContent:"left", verticalAlign:"center"}}>
-                {name === 'home'
-                    ? <HomeIcon sx={{fontSize: "2.5vh", color:"#57201B"}}/>
-                    : <CategoryIcon sx={{fontSize: "2.5vh", color:"#57201B", verticalAlign:"center"}}/>
-                }
-                {expand
-                    ? <Typography variant="h5" sx={{marginLeft:"10px"}}>{name}</Typography>
-                    : <></>
-                }
-            </Box>
-            
-        </Button>;
-    }
-
-    const [expand, setExpand] = useState(true);
-    var sidebar_width = "15vw";
-
-    const expandSidebar = () => {
-        setExpand(!expand);
-        if(expand){
-            sidebar_width = "15vw";
-            handleShift("6vw");
-        }
-        else{
-            sidebar_width = "4vw";
-            handleShift("17vw");
-        }
-    }
-
-        return <Box sx = {{
-                            height: "95vh", 
-                            top: "5",
-                            width: {sidebar_width}, 
-                            display: "block", 
-                            position: "fixed",
-                            borderRight: "solid 1px"
-                            }}
-                >
-            
-            <ListIcon sx = {{margin:"8px 0 0 8px", fontSize: "4vh", color: "#57201B"}} onClick={expandSidebar}/>
-
-            <>{button_sidebar("home")}</>
-            <>{button_sidebar("categories")}</>
-        </Box>
-=======
 import React, { useState } from 'react';
 import { Box, Button, Typography } from '@mui/material';
 import ListIcon from '@mui/icons-material/List'
@@ -114,5 +54,4 @@ export const SIDEBAR = ({handleShift}) => {
             <>{button_sidebar("home")}</>
             <>{button_sidebar("categories")}</>
         </Box>)
->>>>>>> b588b28477f2b8a36db28f2a43a538b132170e45
 }
