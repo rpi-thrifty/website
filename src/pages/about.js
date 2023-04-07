2d9b190b6cdd3caf5656f51a629d3f3d43711d82
Alex Rougebec <arougebec22@gmail.com>
2023-04-07 15:51:56 -0400

    hacker effect final edition

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,12 +2,104 @@ import React from 'react';
 import { Header } from "../components/header/header";
 import { Box, Button } from "@mui/material"
 import "./about.css"
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+document.addEventListener("DOMContentLoaded", function(){
+    observe('.description');
+    observe('.a');
+});
+
+function observe(selector){
+    const myElement = document.querySelector(selector);
+    const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            if(myElement.innerText.length < 20){
+                hackereffectslow(selector);
+                setTimeout(() => {
+                    hackereffectslow('.b');
+                }, 750);
+                setTimeout(() => {
+                    hackereffectslow('.c');
+                }, 1300);
+                setTimeout(() => {
+                    hackereffectslow('.d');
+                }, 2300);
+                setTimeout(() => {
+                    hackereffectslow('.e');
+                }, 3050);
+                observer.disconnect();
+            }
+            else{
+                hackereffectfast(selector);  
+                observer.disconnect();
+            }  
+        }
+    });
+    });
+    observer.observe(myElement);
+}
+
+function hackereffectfast(selector){
+        const target = document.querySelector(selector);
+        if(target){
+            let iterations = 0;
+
+            const interval = setInterval(() => {
+                target.innerText = target.innerText.split("")
+                .map((letter, index) => {
+                    if(target.dataset.value[index] == " "){
+                        return target.dataset.value[index];
+                    }
+                    if(index < iterations){ 
+                        return target.dataset.value[index];
+                    }
+                    return letters[Math.floor(Math.random() * 26)];
+                })
+                .join("")
+
+                if(iterations >= 550){
+                    clearInterval(interval);
+                    iterations = 0;
+                }
+
+                iterations += 1;
+            }, 10);   
+    }
+};
+
+function hackereffectslow(selector){
+    const target = document.querySelector(selector);
+    if(target){
+        let iterations = 0;
+
+        const interval = setInterval(() => {
+            target.innerText = target.innerText.split("")
+            .map((letter, index) => {
+                if(target.dataset.value[index] == " "){
+                    return target.dataset.value[index];
+                }
+                if(index < iterations){ 
+                    return target.dataset.value[index];
+                }
+                return letters[Math.floor(Math.random() * 26)];
+            })
+            .join("")
+
+            if(iterations >= 550){
+                clearInterval(interval);
+                iterations = 0;
+            }
+
+            iterations += 1;
+        }, 50);   
+}
+};
 
 export const AboutPage = () => {
     return(
         <Box sx={{marginLeft:"2.55%"}}> 
             <Header/>
-            
             <div class = "row " id = "sell-section">
                 <div class = "sell-section-left">
                     <img class="sell-image-left" src="/images/sell.jpg" alt="Sell"/>
@@ -15,7 +107,7 @@ export const AboutPage = () => {
                 <div class = "sell-section-right-wrapper">
                     <div class = "sell-section-right">
                         <h2 id="sell text"> Sell an Item! </h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis elit et vehicula ullamcorper. Nulla id metus scelerisque, vestibulum sem eu, feugiat ante.</p>
+                        <h4 data-value='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis elit et vehicula ullamcorper. Nulla id metus scelerisque, vestibulum sem eu, feugiat ante.'class='selltext'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis elit et vehicula ullamcorper. Nulla id metus scelerisque, vestibulum sem eu, feugiat ante.</h4>
                         <Button class="sell-button">Sell</Button>
                     </div>
                 </div>
@@ -24,7 +116,7 @@ export const AboutPage = () => {
                 <div class="buy-section-left-wrapper">
                     <div class = "buy-section-left">
                         <h2 id="buy text"> Buy an Item! </h2>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis elit et vehicula ullamcorper. Nulla id metus scelerisque, vestibulum sem eu, feugiat ante.</p>
+                        <h4 data-value='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis elit et vehicula ullamcorper. Nulla id metus scelerisque, vestibulum sem eu, feugiat ante.'class='buytext'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis elit et vehicula ullamcorper. Nulla id metus scelerisque, vestibulum sem eu, feugiat ante.</h4>
                         <Button class="buy-button">Buy</Button>
                     </div>
                 </div>
@@ -34,7 +126,8 @@ export const AboutPage = () => {
             </div>
             <div class = "row" id = "project-description">
                 <div class = "project-wrapper">
-                <p><b>  Welcome to Thrifty! We are an ecommerce website created 
+                <h4 data-value = 'Welcome to Thrifty! We are an ecommerce website created to facilitate the many transactions made between RPI students annually. Our vision this year is to create a platform where students can post miscellaneous items and find interested buyers. This project creates a solution for graduating students who have extra school supplies and little storage space as well as freshmen with a tight budget. This allows freshmen to save money by buying second hand products at a discounted price, sort of like a permanent online garage sale.' class = 'description'>
+                    Welcome to Thrifty! We are an ecommerce website created 
                     to facilitate the many transactions made between RPI students 
                     annually. Our vision this year is to create a 
                     platform where students can post miscellaneous 
@@ -44,35 +137,27 @@ export const AboutPage = () => {
                     as well as freshmen with a tight budget. This allows freshmen to 
                     save money by buying second hand products at a discounted price, 
                     sort of like a permanent online garage sale.
-                </b></p>
+                </h4>
                 </div>
             </div>
             <div class = "row" id = "about-section">
-                <div class = "members-box">
-                    <h3 id="team-members-title">Team Members: </h3>
-                    <ul>
-                        <li>Alex Rougebec</li>
-                        <li>Jiawei Wu</li>
-                        <li>Nikhil Belgaonkar</li>
-                        <li>Jericho Dizon</li>
-                        <li>Andrew Hua</li>
-                    </ul>
+                <div id = "team-members-title-box">
+                    <h1 id='team'>Meet The Team</h1>
                 </div>
-                <div class="contact-wrapper">
-                    <div class = "contact-box">
-                        <a href = "https://github.com/rpi-thrifty/website">
-                            <img class="aboutimg" src="/images/GitHub-Mark.png" alt="github"/>
-                        </a>
-                        <a href = "https://google.com">
-                            <img class="aboutimg" src="/images/discord.png" alt="discord"/>
-                        </a>
-                    </div>
+                <div id = "team-members">
+                    <h5 data-value = 'Alex Rougebec' class = 'a'>AAAA AAAAAAAA</h5>
+                    <h5 data-value = 'Jiawei Wu' class = 'b'> AAAAAA AA</h5>
+                    <h5 data-value = 'Nikhil Belgaonkar' class = 'c'>AAAAAA AAAAAAAAAA</h5>
+                    <h5 data-value = 'Jericho Dizon' class = 'd'>AAAAAAA AAAAA</h5>
+                    <h5 data-value = 'Andrew Hua' class = 'e'>AAAAAA AAA</h5>
                 </div>
             </div>
         </Box>
-            
+        
     )
 }
+
+
 
 /*
 circle image
