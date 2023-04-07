7124d1cbdf803f886517414983821f48c4e39447
andrewh104 <andrewh1048596@gmail.com>
2023-04-07 17:20:24 -0400

    added search bar
    made a new box for search bar, removed the ":LoginIcon". Added classes in CSS and styled search bar. Added a constant to redirect to another page based on user input.
    
    Changes to categories/logos are from a merge commit conflict

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,35 +1,50 @@
 import * as React from 'react';
-import { Box } from '@mui/material';
+import { Box, Menu, MenuItem } from '@mui/material';
+import { useState } from 'react';
 import LoginIcon from '@mui/icons-material/Login';
 
 import "./header.css";
 
-
 export const Header = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
 
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleSearch = (event) => { { /* redirects to another page based on user input */}
+        event.preventDefault(); { /* ensures no return to current page */}
+        const searchInput = event.target.elements.searchInput.value;
+        window.location.href = `/search/${searchInput}`; { /* search output */}
+    }
 
     return(
         <Box className="bar">
             {/* left */}
             <Box className='group' marginLeft={"20vw"}>
                 <a href='/' className='header-logo'>
-                    <img src={process.env.PUBLIC_URL + 'thrifty_logos/NEW_Thrifty_Mini_Logo.png'} className="header-logo the-logo" title='Home_page' alt=""/>
+                    <img src={process.env.PUBLIC_URL + 'images/logo_transparent.png'} className="header-logo the-logo" title='Home_page' alt=""/>
                 </a>
                 <Box className='header-text'>
                     <a className='header_link' href='/about'>About</a>
-                    <a className='header_link' href='/categories'>Categories</a>
+                    <a className='header_link' href=''>Categories</a>
                     <a className='header_link' href='https://github.com/rpi-thrifty'>Github</a>
                     <a className='header_link' href='https://discord.gg/PFqRbcPq'>Discord</a>
                 </Box>
                 
             </Box>
 
-            {/* right */}
-            <Box className='group' marginRight={"20vw"}>
-                <Box className="header-text">
-                    <LoginIcon/>
-                </Box>
+            {/* right (search bar) */}
+            <Box className='group search-form' marginRight={"10vw"}> {/* might change margins */}
+                <form onSubmit={handleSearch}>
+                    <input type="text" name="searchInput" placeholder="Search..." className="search-input" />
+                </form>
             </Box>
+
         </Box>
     )
 }
